3bcb06eed5dbf34a52c8788af19407056143b3cc
alissacheng <alissabc@hotmail.com>
2024-03-04 22:44:22 -0500

    update documentation and remove setup file

--- transform/northwind/models/staging/orders.sql
+++ transform/northwind/models/staging/orders.sql
@@ -18,7 +18,7 @@ select
     ship_region,
     ship_address,
     ship_country,
-    shipped_date,
+    cast(shipped_date as date) as shipped_date,
     required_date,
     ship_postal_code,
     cast(_airbyte_extracted_at as datetime) as last_update
